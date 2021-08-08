cd663cd11a94e76823ee8ce5151aa232ac2691c2
iconsumeplutonium <uzymc242@outlook.com>
2021-08-08 12:41:46 -0700

    updated scripts because github bad

--- SolarSystem/Assets/SolSystemGen.cs
+++ SolarSystem/Assets/SolSystemGen.cs
@@ -10,7 +10,10 @@ public class SolSystemGen : MonoBehaviour {
     public Transform sunPos;
 
     public Material sunMat;
-    public Material planetMat;
+    public Material planetMat_terrestrial;
+    public Material planetMat_gasGiant;
+
+    public bool autoUpdate;
 
     private void Start() {
         system = new SolarSystem();
@@ -22,28 +25,33 @@ public class SolSystemGen : MonoBehaviour {
         //Create the solar system
         System.Random prng = new System.Random(seed);
 
-        float size = (prng.Next(1, 100) / 100f) * 5f;
-        system.star = new Star(size, Color.yellow);
+        //stars are of size 3 to 5
+        float size = (prng.Next(60, 100) / 100f) * 5f;
+        system.star = new Star(size);
         system.planets = new Planet[prng.Next(0, 10)];
 
         System.Random rng = new System.Random(seed);
         for (int i = 0; i < system.planets.Length; i++) {
 
-            float pSize = (rng.Next(1, 100) / 100f) * 2f;
-            float dist = (rng.Next(1, 100) / 100f) * 5f;
+            int type = rng.Next(0, 2);
+            //terrestrial size 0.02 to 2, gas giant size 2.02 to 3;
+            float pSize = (type == 0) ? (rng.Next(1, 100) / 100f) * 2f : (rng.Next(101, 150) / 100f) * 2f;
+            float dist = (i == 0) ? 8.54f - (system.star.size / 2f) - pSize /*- offset*/ : (rng.Next(1, 100) / 100f) * 5f;
             int numMoons = rng.Next(1, 3);
 
-            system.planets[i] = new Planet(pSize, dist, numMoons);
+            system.planets[i] = new Planet(pSize, dist, numMoons, type);
         }
 
         //Spawn it in
         GameObject star1 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         star1.transform.position = sunPos.transform.position;
         star1.transform.localScale *= system.star.size;
-        star1.transform.localScale = new Vector3(star1.transform.localScale.x, star1.transform.localScale.y + 1, star1.transform.localScale.z);
+        //star1.transform.localScale = new Vector3(star1.transform.localScale.x, star1.transform.localScale.y + 1, star1.transform.localScale.z);
         sunMat.color = system.star.c;
         star1.GetComponent<MeshRenderer>().material = sunMat;
         star1.name = "Star";
+        star1.isStatic = true;
+        star1.tag = "Celestial";
 
         Vector3 p = sunPos.transform.position;
         float xPos = star1.transform.position.x;
@@ -56,11 +64,15 @@ public class SolSystemGen : MonoBehaviour {
             o.transform.position = p;
 
             o.transform.localScale *= system.planets[i].size;
-            if(i == system.planets.Length - 1 && p.x < -36.5) {
-                o.transform.localScale = new Vector3(o.transform.localScale.x, o.transform.localScale.y + 1, o.transform.localScale.z);
-            } //make it so that small sun sizes cant happen
-            o.GetComponent<MeshRenderer>().material = planetMat;
-            o.name = "Planet " + (i + 1);
+            //if(i == system.planets.Length - 1 && p.x < -36.5) {
+            //    o.transform.localScale = new Vector3(o.transform.localScale.x, o.transform.localScale.y + 1, o.transform.localScale.z);
+            //} //make it so that small sun sizes cant happen
+            o.GetComponent<MeshRenderer>().material = (system.planets[i].planetType == 0) ? planetMat_terrestrial : planetMat_gasGiant;
+
+            string t = (system.planets[i].planetType == 0) ? " (Terrestrial)" : " (Gas Giant)";
+            o.name = "Planet " + (i + 1) + t;
+            o.isStatic = true;
+            o.tag = "Celestial";
         }
 
 
--- SolarSystem/Assets/SolarSystem.cs
+++ SolarSystem/Assets/SolarSystem.cs
@@ -8,9 +8,9 @@ public class SolarSystem {
     public Planet[] planets;
 
     public void DebugSystem() {
-        string v = "Star is of size " + star.size + ". It has " + planets.Length + " planets.";
+        string v = "Star () is of size " + star.size + ". It has " + planets.Length + " planets.";
         for (int i = 0; i < planets.Length; i++) {
-            v += " Planet " + i + " is of size " + planets[i].size + ", is " + planets[i].distFromHost + " away from the sun, and has " + planets[i].numMoons + " moons.";
+            v += " Planet " + (i + 1) + " is of size " + planets[i].size + ", is " + planets[i].distFromHost + " away from the sun, and has " + planets[i].numMoons + " moons.";
         }
         Debug.Log(v);
 
@@ -22,10 +22,53 @@ public class Star {
 
     public float size;
     public Color c;
+    public char classification;
 
-    public Star(float s, Color c) {
+    public Star(float s) {
         size = s;
-        this.c = c;
+        //this.c = c;
+    }
+
+    public void GenerateExtraneousDetails() {
+        //size is its diameter.
+        //stars are of size 3 to 5. There are 7 classifications of star, thus each threshold increases by 2/7 or 0.28
+        c = GetColor();
+        Debug.Log(classification);
+    }
+
+    private Color GetColor() {
+        Color c = Color.white;
+
+        if (size > 3f && size <= 3.28f) {
+            c = new Color(255, 189, 111);
+            classification = 'M';
+        }
+        else if (size > 3.28f && size <= 3.56f) {
+            c = new Color(255, 221, 180);
+            classification = 'K';
+        } 
+        else if(size > 3.56f && size <= 3.84f) {
+            c = new Color(255, 244, 232);
+            classification = 'G';
+        } 
+        else if(size > 3.84f && size <= 4.12f) {
+            c = new Color(251, 248, 255);
+            classification = 'F';
+        }
+        else if(size > 4.12f && size <= 4.4f) {
+            c = new Color(202, 216, 255);
+            classification = 'A';
+        }
+        else if (size > 4.12f && size <= 4.4f) {
+            c = new Color(170, 191, 255);
+            classification = 'B';
+        }
+        else if (size > 4.12f && size <= 4.4f) { //this doesnt go all the way up to 5, check on that
+            c = new Color(155, 176, 255);
+            classification = 'O';
+        }
+
+        return c;
     }
 }
 
@@ -33,11 +76,12 @@ public class Planet {
     public float size;
     public float distFromHost;
     public int numMoons;
+    public int planetType; //0 for terrestrial, 1 for gas giant
 
-    public Planet(float size, float distFromHost, int numMoons)
-    {
+    public Planet(float size, float distFromHost, int numMoons, int type) {
         this.size = size;
         this.distFromHost = distFromHost;
         this.numMoons = numMoons;
+        this.planetType = type;
     }
 }
